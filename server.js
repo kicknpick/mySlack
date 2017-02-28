2dbc8df478f16f9f4e691a98de077d351811227b
kicknpick <travisbpickering@gmail.com>
2017-02-27 21:43:39 -0600

    slack is working

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(express.static("./public"));
 
 // Routes =============================================================
 
-require("./routes/slackRoutes.js")(app);
-require("./routes/userRoutes.js")(app);
+// require("./routes/slackRoutes.js")(app);
+// require("./routes/userRoutes.js")(app);
 // require("./routes/slackbot.js")(app);
 
 
@@ -30,22 +30,46 @@ var Slackbot = require('slackbots');
 
 // create a bot 
 var bot = new Slackbot({
-	token: process.env.slacktoken,
+    // token: process.env.slacktoken,
+    token: 'xoxb-146588198321-AjAKw6BhR2Yhjvl6UEFzjn5A',
     name: 'mytest'
 });
 
 bot.on('message', function(data) {
-	console.log(data.text);
-	if (data.text === "save") {
-		console.log("it worked!!!!");
-	}
+    if (data.type === "message") {
+
+
+        //if the message is a file, do something
+        if (data.file) {
+            console.log("This is file shared");
+            console.log(data);
+            //processing the filen to send to mySQL
+        }
+
+        if (data.text && !data.file) {
+
+        	var test = data.text.indexOf("#save");
+
+        	if (test > -1) {
+        		console.log("we are the best");
+        	}
+
+            console.log("This is text");
+            console.log(data.text);
+            // console.log(data.message.user);
+            // console.log(data.message.text);
+        }
+
+
+    };
+    return
 });
 
 
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
